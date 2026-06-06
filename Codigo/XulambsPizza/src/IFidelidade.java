f4f08540843faef2221a1daf1df98c60ced5f0f4
Joao Caram <caram@pucminas.br>
2026-06-06 19:13:25 -0300

    fix: Valores da Fidelidade

--- Codigo/XulambsPizza/src/IFidelidade.java
+++ Codigo/XulambsPizza/src/IFidelidade.java
@@ -3,12 +3,13 @@ import java.util.List;
 
 public interface IFidelidade {
     static final int[] QUANTIDADES = {0,10,20};
-    static final int[] VALORES =     {0,29,44};
+    static final double[] VALORES =  {0,29,44};
     
     public double descontoPedido(Pedido pedido);
     
     public static IFidelidade definirCategoria(List<Pedido> pedidos){
-        IFidelidade[] categorias = {new XulambsJunior(), new XulambsPleno(), new XulambsSenior()}; 
+        IFidelidade[] categorias = 
+            {new XulambsJunior(), new XulambsPleno(), new XulambsSenior()}; 
         IFidelidade resposta = null;
         int quantidade = pedidos.size();
         double valor = 0d;
@@ -16,7 +17,7 @@ public interface IFidelidade {
             valor += pedido.precoAPagar();
         }
         double valorMedio = valor/quantidade;
-        int pos = 2;
+        int pos = VALORES.length-1;
         while(quantidade < QUANTIDADES[pos] && pos >= 0){
                pos--; 
         }
